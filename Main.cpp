ecf13f87c6a152356cc4c28dee2f4652f3530459
Otour, Burhan <botour@mail.uni-paderborn.de>
2019-07-07 18:56:31 +0200

    Add gitignore file:

---
+++ Main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by botour on 07.07.19.
+//
+
+#include "Main.h"
---
+++ src/Trie.h
@@ -0,0 +1,8 @@
+//
+// Created by botour on 07.07.19.
+//
+
+#ifndef ADS_TRIE_H
+#define ADS_TRIE_H
+
+#endif //ADS_TRIE_H
